724b21b34d061dc912911ceac47f72682e0cb4ef
Armin Kenel <armin-kenel@users.noreply.github.com>
2023-04-12 10:24:12 +0200

    enhance README.md

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Notes2GoogleInterface
 Defines the interface between exported data from Lotus Notes and the imported data into Google.
 
-# class
+## class
 Java class <code>InterfacePerson</code>.
+
+## services
+ - <code>ReadWriteInterfacePerson.writeJson()</code>
+ - <code>ReadWriteInterfacePerson.readJson()</code>
--- src/ch/secona/notes2google/ReadWriteInterfacePerson.java
+++ src/ch/secona/notes2google/ReadWriteInterfacePerson.java
@@ -12,10 +12,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class ReadWriteInterfacePerson {
-	public final static String PATH = "nge_interfacePersonList.json";
+	public static String PATH = "nge_interfacePersonList.json";
+
+	public static void writeJson(final String path, final List<InterfacePerson> interfacePersonList) {
+		PATH = path;
+		writeJson(interfacePersonList);
+	}
 
 	public static void writeJson(final List<InterfacePerson> interfacePersonList) {
-
 		try (PrintWriter out = new PrintWriter(new FileWriter(PATH))) {
 			final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 			final String jsonString = gson.toJson(interfacePersonList);
@@ -24,6 +28,11 @@ public class ReadWriteInterfacePerson {
 		} catch (final Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static <T> List<T> readJson(final String path, final Class<T[]> clazz) {
+		PATH = path;
+		return readJson(clazz);
 	}
 
 	public static <T> List<T> readJson(final Class<T[]> clazz) {
