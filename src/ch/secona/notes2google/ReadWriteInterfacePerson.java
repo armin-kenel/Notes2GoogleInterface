b936cbd765d2e5db02886d1ad88f2eafa6714776
Armin Kenel <armin.kenel@secona.ch>
2023-08-02 14:38:49 +0200

    Update ReadWriteInterfacePerson.java
    write and read with UTF-8 (so it works for all envrionments and IDEs)

--- src/ch/secona/notes2google/ReadWriteInterfacePerson.java
+++ src/ch/secona/notes2google/ReadWriteInterfacePerson.java
@@ -1,8 +1,10 @@
 package ch.secona.notes2google;
 
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -15,24 +17,31 @@ public class ReadWriteInterfacePerson {
 	public static String PATH = "nge_interfacePersonList.json";
 
 	public static void writeJson(final String path, final List<InterfacePerson> interfacePersonList) {
-		PATH = path;
-		writeJson(interfacePersonList);
+		ReadWriteInterfacePerson.PATH = path;
+		ReadWriteInterfacePerson.writeJson(interfacePersonList);
 	}
 
 	public static void writeJson(final List<InterfacePerson> interfacePersonList) {
-		try (PrintWriter out = new PrintWriter(new FileWriter(PATH))) {
-			final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			final String jsonString = gson.toJson(interfacePersonList);
 
-			out.write(jsonString);
+		try (FileOutputStream fos = new FileOutputStream(ReadWriteInterfacePerson.PATH);
+				OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+				BufferedWriter writer = new BufferedWriter(osw)) {
+			final String jsonString = ReadWriteInterfacePerson.list2String(interfacePersonList);
+
+			writer.append(jsonString);
 		} catch (final Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	private static String list2String(final List<InterfacePerson> interfacePersonList) {
+		final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		return gson.toJson(interfacePersonList);
+	}
+
 	public static <T> List<T> readJson(final String path, final Class<T[]> clazz) {
-		PATH = path;
-		return readJson(clazz);
+		ReadWriteInterfacePerson.PATH = path;
+		return ReadWriteInterfacePerson.readJson(clazz);
 	}
 
 	public static <T> List<T> readJson(final Class<T[]> clazz) {
@@ -40,7 +49,8 @@ public class ReadWriteInterfacePerson {
 
 		try {
 			final Gson gson = new Gson();
-			final Reader reader = Files.newBufferedReader(Paths.get(PATH));
+			final Reader reader = Files.newBufferedReader(Paths.get(ReadWriteInterfacePerson.PATH),
+					StandardCharsets.UTF_8);
 
 			array = gson.fromJson(reader, clazz);
 			reader.close();
